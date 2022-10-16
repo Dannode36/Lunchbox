fec147ffeebbe82c773db94dd5d46081aa90b31a
Dannode36 <russelme848@gmail.com>
2022-10-16 12:41:08 +1100

    SFML working
    - Beginning grid work

--- src/Lunchbox_Editor.cpp
+++ src/Lunchbox_Editor.cpp
@@ -1,6 +1,7 @@
 #include "imgui.h"
 #include "imgui-SFML.h"
 
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
@@ -9,16 +10,27 @@
 
 Overlord gOverlord;
 
+int resX = 1920;
+int resY = 1080;
+
+float gridSize_f = 100.f;
+unsigned gridSize_u = static_cast<unsigned>(gridSize_f);
+
 int main() {
 
     gOverlord.Init();
 
-    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    sf::RenderWindow window(sf::VideoMode(resX, resY), "ImGui + SFML = <3");
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::View view;
+    view.setSize(resX, resY);
+    view.setCenter(window.getSize().x / 2, window.getSize().y / 2);
+
+    //Init Game Elements
+    sf::RectangleShape shape(sf::Vector2f(gridSize_f, gridSize_f));
+
 
     sf::Clock deltaClock;
     while (window.isOpen()) {
@@ -38,7 +50,14 @@ int main() {
         ImGui::End();
 
         window.clear();
+        window.setView(view);
+        //Draw Game;
+        //shape.setPosition(window.getDefaultView().getCenter().x - shape.getRadius(), window.getDefaultView().getCenter().y - shape.getRadius());
         window.draw(shape);
+
+        window.setView(window.getDefaultView());
+        //Draw UI
+
         ImGui::SFML::Render(window);
         window.display();
     }
